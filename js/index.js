3eac17c6378c23226e91603be0df2bdd807f6a8d
Jiaxin Zhao <jzhao537@uwo.ca>
2020-12-12 18:32:03 +0800

    commit rotation charts

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ window.onload = function(){
     var searchIcon = document.getElementById('searchIcon');
     var searchCourse = document.getElementById('searchCourse');
     var searchBox = document.getElementById('search');
-    console.log(searchIcon, searchCourse, searchBox);
+    // console.log(searchIcon, searchCourse, searchBox);
 
     // When the mouse enter into the search icon, display the search box 
     // Onmouseenter will not trigger the event of parent element
@@ -31,5 +31,116 @@ window.onload = function(){
         // search icon displays
         searchIcon.style.visibility = 'visible';
     }
+
+    // 2.Realization of rotation banners
+    // 2.1 Ajax request image address data
+    ajax('get', '../data/banner.json','',function(res){
+        // console.log(res);
+        // transform data
+        var bannerUrl = JSON.parse(res);
+        console.log(bannerUrl);
+
+        // 2.2 Data rendering to the banner
+        // Get banner ul span
+        var banner = document.getElementById('banner');
+        // console.log(banner);
+        var bannerlist = banner.querySelector('.bannerlist');
+        var shiftlist = banner.querySelector('.shiftlist');
+        // console.log(bannerlist, shiftlist);
+
+        // Declare variables to store address
+        var bannerImage = '', shift = '';
+        for(var i = 0; i < bannerUrl.length; i++){
+            // console.log(bannerUrl[i]);
+            bannerImage += '<li><img src="'+ bannerUrl[i] +'" alt="banner"></li>';
+            shift += '<span></span>'
+        }
+        // Rendering to the father element
+        bannerlist.innerHTML = bannerImage;
+        shiftlist.innerHTML = shift;
+
+        // 2.3 Initialize banner styles
+        var spans = shiftlist.getElementsByTagName('span');
+        spans[0].className = 'active';
+
+        // Seamless carousel puls last banner
+        bannerlist.innerHTML += '<li><img src="'+ bannerUrl[0] +'" alt="banner"></li>';
+
+        // Make the banner move
+        // Get the width of banner
+        var cw = banner.clientWidth;
+        // console.log(cw);
+
+        // Recalculate the width of bannerlist
+        bannerlist.style.width = (bannerUrl.length + 1) * cw + 'px';
+        
+        // Suppose the picture is the first one
+        var n = 0;
+        // Distance of moving = index * bannerImage width
+        // Direction : 0 - 1 - 2 - 3 - 4 -5 - 0
+        banner.timer = setInterval(auto, 4000);
+        // enter the banner, clear interval
+        banner.onmouseenter = function(){
+            clearInterval(banner.timer);
+        }
+        // leave the banner, activate the timer
+        banner.onmouseleave = function(){
+            banner.timer = setInterval(auto, 4000);
+        }
+        // Click shift button, shift to corresponding banner image
+        for(var i = 0; i < spans.length; i++){
+            // Custom index
+            spans[i].index = i;
+            // Add click event to each span
+            spans[i].onclick = function(){
+                n = this.index - 1;
+                auto();
+            }
+        }
+        // Click right button, shift to the next banner image
+        var btns = banner.getElementsByTagName('button');
+        // console.log(btns);
+
+        btns[1].onclick = function(){
+            auto();
+        }
+
+        // Click left button, shift to the previous banner image
+        btns[0].onclick = function(){
+            n -= 2;
+            auto();
+        }
+        // auto()
+        function auto(){
+            // Index ++
+            n++;
+
+            // If index gets to the last one, return the first one
+            if(n > bannerUrl.length){
+                n = 0;
+                // Change position 
+                bannerlist.style.left = n * (-cw) + 'px';
+                // Update index
+                n = 1;
+            }
+            // If index gets to the first one, return the last one
+            if(n < 0){
+                n = bannerUrl.length - 1;
+                // flash
+                bannerlist.style.left = n * (-cw) + 'px';
+            }
+
+            // Call move(ele, attr, step, target) function
+            move(bannerlist, 'left', 50, n * (-cw));
+
+            // Exclusive operation
+            for(var i = 0; i < spans.length; i++){
+                spans[i].className = '';
+            }
+
+            // shift and bannerImage corresponds to each other
+            spans[n == bannerUrl.length ? 0 : n].className = 'active';
+        }
+    });
 
 }
