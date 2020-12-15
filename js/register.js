e1259866f1067ee1579dd1ff0679389f7b87d636
Jiaxin Zhao <jzhao537@uwo.ca>
2020-12-15 17:50:05 +0800

    update register.js

--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     // Get goodCourse and course type in top nav
     var goodCourse = document.getElementById('goodcourse');
     // console.log(goodCourse);
@@ -6,70 +6,72 @@ window.onload = function(){
     // console.log(courseType);
     // When mouse enter into goodCourse, display course type,
     // Otherwise hide courseType
-    goodCourse.onmouseenter = function(){
+    goodCourse.onmouseenter = function () {
         courseType.style.display = 'block';
     }
-    courseType.onmouseleave = function(){
+    courseType.onmouseleave = function () {
         this.style.display = 'none';
     }
     /* 
         Slide the mouse into the search icon to 
         display the entire search box
     */
-     // get the search icon, search course and search box element
-     var searchIcon = document.getElementById('searchIcon');
-     var searchCourse = document.getElementById('searchCourse');
-     var searchBox = document.getElementById('search');
-     // console.log(searchIcon, searchCourse, searchBox);
- 
-     // When the mouse enter into the search icon, display the search box 
-     // Onmouseenter will not trigger the event of parent element
-     searchIcon.onmouseenter = function(){
-         // display the search box, visiblity = visible
-         searchBox.style.visibility = 'visible';
-         searchCourse.style.visibility = 'visible';
-         // auto focus ele.focus()
-         searchCourse.focus();
-     }
- 
-     // Click on the blank space, the search box disappears, leaving only the search icon
-     // Consider stop propagation
-     document.onclick = function(evs){
-         // Consider compatible
-         var ev = window.event || evs;
-         ev.stopPropagation ? ev.stopPropagation():ev.cancelBubble = true;
-         // searchBox disappears
-         searchBox.style.visibility = 'hidden';
-         searchCourse.style.visibility = 'hidden';
-         // search icon displays
-         searchIcon.style.visibility = 'visible';
-     }
+    // get the search icon, search course and search box element
+    var searchIcon = document.getElementById('searchIcon');
+    var searchCourse = document.getElementById('searchCourse');
+    var searchBox = document.getElementById('search');
+    // console.log(searchIcon, searchCourse, searchBox);
 
-     /* 
-        login verification
-     */
+    // When the mouse enter into the search icon, display the search box 
+    // Onmouseenter will not trigger the event of parent element
+    searchIcon.onmouseenter = function () {
+        // display the search box, visiblity = visible
+        searchBox.style.visibility = 'visible';
+        searchCourse.style.visibility = 'visible';
+        // auto focus ele.focus()
+        searchCourse.focus();
+    }
+
+    // Click on the blank space, the search box disappears, leaving only the search icon
+    // Consider stop propagation
+    document.onclick = function (evs) {
+        // Consider compatible
+        var ev = window.event || evs;
+        ev.stopPropagation ? ev.stopPropagation() : ev.cancelBubble = true;
+        // searchBox disappears
+        searchBox.style.visibility = 'hidden';
+        searchCourse.style.visibility = 'hidden';
+        // search icon displays
+        searchIcon.style.visibility = 'visible';
+    }
+
+    /* 
+       login verification
+    */
     // Get form and em
+
     var registerForm = document.getElementById('registerform');
     var marks = registerForm.getElementsByTagName('em');
     // console.log(marks);
+
     // Get username 
     var username = registerform.username;
     // console.log(username);
     // phone regExp
     var userReg = /^1[3-9]\d{9}$/;
-    
+
     judgeNull(username, marks[0], '请输入手机!');
 
-    username.onchange = function(){
+    username.onchange = function () {
         // change color
         marks[0].style.color = '#FF6600';
         // judge regExp
         // if it is the wrong value
-        if(!userReg.test(this.value)){
+        if (!userReg.test(this.value)) {
             marks[0].innerHTML = '账号格式不正确!'
             return false;
         }
-        else{
+        else {
             marks[0].innerHTML = '<i class = "iconfont icon-dui1"></i>'
         }
     }
@@ -84,26 +86,26 @@ window.onload = function(){
     // low version: all number or all alphabets
     var lowReg = /^\d{6,18}$|^[a-zA-Z]{6,18}$/;
     // medium version: number + alphabets, first is letter
-    var mediumReg  = /(?!^[a-zA-Z]{6,18}$)^[a-zA-Z][0-9a-zA-Z]{5,17}$/;
+    var mediumReg = /(?!^[a-zA-Z]{6,18}$)^[a-zA-Z][0-9a-zA-Z]{5,17}$/;
     // high version: number + alphabets + symbols, first is letter
     var highReg = /(?!^[!@#$%^&*()_+-=]{6,18}$)(?!^\d{6,18}$)(?!^[a-zA-Z]{6,18}$)^[a-zA-Z][0-9a-zA-Z!@#$%^&*()_+-=]{5,17}$/;
 
-    password.onchange = function(){
+    password.onchange = function () {
         // change color
         marks[1].style.color = '#ff6600';
         // first judge the length
-        if(password.value.length < 6 || password.value.length > 18){
+        if (password.value.length < 6 || password.value.length > 18) {
             marks[1].innerHTML = '密码应该为6-20位之间!'
             return false;
         }
         // Judge three version of password
-        if(lowReg.test(this.value)){
+        if (lowReg.test(this.value)) {
             marks[1].innerHTML = '低';
-        }else if(mediumReg.test(this.value)){
+        } else if (mediumReg.test(this.value)) {
             marks[1].innerHTML = '中';
-        }else if(highReg.test(this.value)){
+        } else if (highReg.test(this.value)) {
             marks[1].innerHTML = '高';
-        }else{
+        } else {
             marks[1].innerHTML = '密码格式不正确!';
             return false;
         }
@@ -123,34 +125,30 @@ window.onload = function(){
     // get random number button
     var randomCode = document.getElementById('code');
     // console.log(randomCode);
-    var newCode = getRandomCode(5);   
+    var newCode = getRandomCode(5);
     randomCode.innerHTML = newCode;
     verificationEqual(verification, newCode, marks[3], '验证码错误!');
     // When click the randomCode button, generate new code
-    randomCode.onclick = function(){
-        newCode = getRandomCode(5);   
+    randomCode.onclick = function () {
+        newCode = getRandomCode(5);
         this.innerHTML = newCode;
         verificationEqual(verification, newCode, marks[3], '验证码错误!');
     }
-    
-    judgeNull(verification, marks[3],'请输入验证码!');
-    
-    resetNull(verification, marks[3]);
+
+    judgeNull(verification, marks[3], '请输入验证码!');
 
     // get message, getmessage
     var message = document.getElementById('message');
     var getMessage = document.getElementById('getmessage');
     // console.log(message);
-    getMessage.onclick = function(){
+    getMessage.onclick = function () {
         // get random random code
         var messageCode = getRandomCode(6);
         alert(messageCode);
         // judgeEqual(message, messageCode, marks[4],'验证码不正确!');
-        verificationEqual(message, messageCode,marks[4],'验证码错误!');
+        verificationEqual(message, messageCode, marks[4], '验证码错误!');
     }
-    judgeNull(message, marks[4],'请输入验证码!');
-    
-    resetNull(message, marks[4]);
+    judgeNull(message, marks[4], '请输入验证码!');
 
     /* 
      Store user data into local storage
@@ -159,10 +157,10 @@ window.onload = function(){
     // If there is no array, create an array to store the data source
 
     var userArr = [];
-    if(!localStorage.userArr){
+    if (!localStorage.userArr) {
         // use not exist, create userArr in cache
         localStorage.userArr = JSON.stringify(userArr);
-    }else{
+    } else {
         // user exists, save existed user information
         userArr = JSON.parse(localStorage.userArr);
     }
@@ -171,21 +169,31 @@ window.onload = function(){
 
     // Get register button
     var registerButton = registerForm.register;
-    console.log(registerButton);
-
-    
-
-    
+    // console.log(registerButton);
 
 
     
+    // If all the verification passes, user info correct
+    // register user
+    var username = registerForm.username.value;
+    var password = registerForm.password.value;
+    console.log(username, password);
+
+    return false;
 
 
-    function judgeNull(name, mark, content){
-        name.onblur = function(){
+
+
+
+
+
+
+
+    function judgeNull(name, mark, content) {
+        name.onblur = function () {
             // if the value is '' 
             // console.log(username.value);
-            if(this.value == ''){
+            if (this.value == '') {
                 mark.innerHTML = content;
                 mark.style.color = '#999999';
                 return false;
@@ -193,39 +201,39 @@ window.onload = function(){
         }
     }
 
-    function judgeEqual(pre, next ,mark, content){
-        pre.onchange = function(){
+    function judgeEqual(pre, next, mark, content) {
+        pre.onchange = function () {
             // change color
             mark.style.color = '#ff6600';
-            
-            if(this.value != next.value){
+
+            if (this.value != next.value) {
                 mark.innerHTML = content;
-            }else{
-                mark.innerHTML = '<i class = "iconfont icon-dui1"></i>'; 
+            } else {
+                mark.innerHTML = '<i class = "iconfont icon-dui1"></i>';
             }
         }
     }
 
-    function verificationEqual(pre, next, mark, content){
-        pre.onchange = function(){
+    function verificationEqual(pre, next, mark, content) {
+        pre.onchange = function () {
             mark.style.color = '#ff6600';
-            if(this.value != next){
+            if (this.value != next) {
                 mark.innerHTML = content;
                 return false;
-            }else{
-                mark.innerHTML = '<i class = "iconfont icon-dui1"></i>'; 
+            } else {
+                mark.innerHTML = '<i class = "iconfont icon-dui1"></i>';
             }
         }
     }
 
-    function resetNull(name, mark){
-        name.onchange = function(){
+    function resetNull(name, mark) {
+        name.onchange = function () {
             mark.style.color = '#ff6600';
             mark.innerHTML = '';
         }
     }
 
-    function getRandomCode(n){
+    function getRandomCode(n) {
         // n: the digit that we want
         var str = '0123456789';
         // save random code
@@ -236,5 +244,5 @@ window.onload = function(){
         }
         return s;
     }
-    
+
 }
