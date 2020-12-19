f623692afba5e258cda246e48bdf89f1cedb760c
Jiaxin Zhao <jzhao537@uwo.ca>
2020-12-19 21:27:26 +0800

    commit rendering course to the page

--- js/list.js
+++ js/list.js
@@ -82,15 +82,95 @@ window.onload = function () {
     var priceList = priceRange.querySelector('ul');
     // console.log(priceRange,priceLink, priceList);
     // When mouse enter priceLink, display priceList
-    priceLink.onmouseenter =  function(){
+    priceLink.onmouseenter = function () {
+        this.style.backgroundColor = '#ffffff';
         priceList.style.display = 'block';
     }
     // When mouse leave priceList, priceList disappears
-    priceList.onmouseleave = function(){
+    priceList.onmouseleave = function () {
+        priceLink.style.backgroundColor = '';
         priceList.style.display = 'none';
     }
 
-    
+    // Get data from play.json
+    ajax('get', '../data/play.json', '', function (res) {
+        // console.log(res)
+        // Transform to js data
+        var courseUrl = JSON.parse(res);
+        // console.log(courseUrl);
+
+        // calculate number of course 
+        var total = courseUrl.length;
+        // console.log(total);
+
+        // Rendering default page
+        // One page shows 16 courses
+        var pageNum = 0;
+        var lenPerPage = 16;
+
+        // Get maximum page
+        var maxPage = Math.ceil(total / lenPerPage);
+        console.log(maxPage);
+
+        // Rendering page nav
+        // Geting element pagenav, span page
+        var pageNav = document.getElementById('pagenav');
+        var pageSeq = pageNav.getElementsByClassName('pagesequence')[0];
+        // console.log(pageNav, pageSeq);
+        createPageSeq();
+        // The default page is one
+
+        // Rendering course to the page
+        getCourse();
+
+        function getCourse() {
+            // Rendering data to course list
+            // get element courselist
+            var courseList = document.getElementById('courselist');
+            // console.log(courseList);
+
+            // show data we want to 
+            // slice(startIndex, lastIndex)
+            // 0 - 15  16- 31 32 - 48
+            // startIndex: pageNum * lenPerPage
+            // lastIndex: (pageNum+1) * lenPerPage
+            var showCourse = courseUrl.slice(pageNum * lenPerPage, (pageNum + 1) * lenPerPage);
+            console.log(showCourse);
+            var html = '';
+            for (var i = 0; i < showCourse.length; i++) {
+                html += `<li>
+            <a class="img">
+                <img src="${showCourse[i].img}" alt="course">
+            </a>
+            <h3>${showCourse[i].title}</h3>
+            <p>
+                共有<b id = "num">${showCourse[i].num}</b>人在学</span>
+                <em>&nbsp;&nbsp;|&nbsp;&nbsp;</em>
+                <span>￥<b id = "price">${showCourse[i].price}</b></span>
+            </p>
+        </li>`
+            }
+            // append html to courselist
+            courseList.innerHTML = html;
+        }
+
+        function createPageSeq() {
+            var sequence = '';
+            for (var i = 0; i < maxPage; i++) {
+                sequence += '<span>' + (i + 1) + '</span>';
+            }
+            // give content to the pageSeq
+            pageSeq.innerHTML = sequence;
+            // give first page active class
+            var firstPage = pageSeq.getElementsByTagName('span')[0];
+            //console.log(firstPage);
+            firstPage.className = 'active';
+        }
+
+
+
+
+    });
 
 
 
